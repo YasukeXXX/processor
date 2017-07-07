f6a64fed1c4b54535e2fefe0fd2c1a58e4de47cf
Sueyasu Keita <yasukexxx@gmail.com>
2017-07-07 16:47:12 +0900

    Update Gemfile.lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "3.8.2"
 
 set :application, "rails_app"
-# set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:YasukeXXX/processor.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/rails_application"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "192.168.33.11", user: "developer", roles: %w{app db web}, ssh_options: { keys: %w(~/.ssh/id_rsa) }
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -19,7 +19,9 @@
 
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app,  %w{developer@192.168.33.11}
+role :web,  %w{developer@192.168.33.11}
+role :db,  %w{developer@192.168.33.11}
 
 
 
