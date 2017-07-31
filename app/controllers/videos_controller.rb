a14a59a534f61fdf5004194a8161e835f9e22083
Sueyasu Keita <yasukexxx@gmail.com>
2017-07-31 10:09:28 +0900

    Move video upload from controller to model

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,7 +6,6 @@ class VideosController < ApplicationController
 
   def create
     @user = User.find_by(id: params[:user_id])
-    params[:video][:path] = uploader.return_path_after_store(params[:video][:video])
     @user.videos.create!(video_params)
   end
 
@@ -18,10 +17,6 @@ class VideosController < ApplicationController
   private
 
   def video_params
-    params.require(:video).permit(:path)
-  end
-
-  def uploader
-    @uploader ||= VideoUploader.new
+    params.require(:video).permit(:video)
   end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -4,7 +4,21 @@ class Video < ApplicationRecord
   belongs_to :user
   has_many :fragments, dependent: :destroy
 
+  attr_accessor :video
+
+  before_create :upload_video
+
   def videos_id
     File.join(path.split('/').slice(2..-1))
+  end
+
+  private
+
+  def upload_video
+    path = uploader.return_path_after_store(video)
+  end
+
+  def uploader
+    @uploader ||= VideoUploader.new
   end
 end
